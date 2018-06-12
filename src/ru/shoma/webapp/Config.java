5143db4d934ee3ec53d8febcaa9e53fa98cc3099
soufee <soufee@mail.ru>
2018-06-12 20:51:10 +0300

    with property file

--- src/ru/shoma/webapp/Config.java
+++ src/ru/shoma/webapp/Config.java
@@ -7,7 +7,7 @@ import java.io.*;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File("./config/resumes.properties");
+    private static final File PROPS = new File("./src/ru/shoma/webapp/config/resumes.properties");
     private static final Config INSTANCE = new Config();
     private final File storageDir;
     private Storage storage;
@@ -25,13 +25,15 @@ public class Config {
     }
 
     private Config() {
+        System.out.println(PROPS.getAbsolutePath());
         try (InputStream is = new FileInputStream(PROPS)) {
             Properties props = new Properties();
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
             storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
-  } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+            System.out.println("Storage created "+storage);
+        } catch (IOException e) {
+            throw new IllegalStateException("Invalid ru.shoma.webapp.config file " + PROPS.getAbsolutePath());
         }
     }
 }
--- src/ru/shoma/webapp/web/ResumeServlet.java
+++ src/ru/shoma/webapp/web/ResumeServlet.java
@@ -1,19 +1,27 @@
 package ru.shoma.webapp.web;
 
+import ru.shoma.webapp.Config;
+import ru.shoma.webapp.storage.Storage;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import java.io.IOException;
 
 public class ResumeServlet extends HttpServlet {
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Storage storage = Config.getInstance().getStorage();
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
         String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello, Ashamaz!" : "Hello, " + name + "!");
+        if (name == null) response.getWriter().write(String.valueOf(storage.getAllSorted()));
+        else response.getWriter().write(String.valueOf(storage.get(name)));
     }
+
+
 }
